35f3fc09560deb9f41d1ce414c8f5f32081b92be
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-04-04 21:17:42 -0600

    Ammended mistake

--- com.autovend.hardware_0.3/src/com/autovend/devices/observers/TouchScreenObserver.java
+++
@@ -1,9 +0,0 @@
-package com.autovend.devices.observers;
-
-import com.autovend.devices.AbstractDevice;
-
-/**
- * Represents listeners that respond only to enable/disable events on touch screens.
- */
-public interface TouchScreenObserver extends AbstractDeviceObserver {
-}
