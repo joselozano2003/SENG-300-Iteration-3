93cac2bba7ba83045e2d045bda20de9b804357b2
benjaminfrey363 <109392270+benjaminfrey363@users.noreply.github.com>
2023-04-06 11:38:23 -0600

    Added bag approval events

--- com.autovend.software/src/gui/AttendantInterface.java
+++ com.autovend.software/src/gui/AttendantInterface.java
@@ -17,24 +17,28 @@ public class AttendantInterface {
 	// second column contains buttons to resolve.
 	int num_stations;
 	JLabel[] discrepancyLabels;
-	JButton[] resolveButtons;
+	JButton[] approveButtons;
+	JButton[] refuseButtons;
 	
 	public AttendantInterface(int number_stations) {
 		num_stations = number_stations;
 		discrepancyLabels = new JLabel[num_stations];
-		resolveButtons = new JButton[num_stations];
+		approveButtons = new JButton[num_stations];
+		refuseButtons = new JButton[num_stations];
 		
 		attendantFrame = new JFrame("Attendant Interface");
 		attendantPanel = new JPanel();
-		attendantPanel.setLayout(new GridLayout(num_stations, 2));
+		attendantPanel.setLayout(new GridLayout(num_stations, 3));
 		
 		// Set up each row. Initially, no weight discrepancies.
 		for (int i = 0; i < num_stations; ++i) {
 			discrepancyLabels[i] = new JLabel("Station " + (i + 1) + ": Working normally...");
-			resolveButtons[i] = new JButton("");
+			approveButtons[i] = new JButton("");
+			refuseButtons[i] = new JButton("");
 			
 			attendantPanel.add(discrepancyLabels[i]);
-			attendantPanel.add(resolveButtons[i]);
+			attendantPanel.add(approveButtons[i]);
+			attendantPanel.add(refuseButtons[i]);
 		}
 		
     	attendantFrame.getContentPane().add(attendantPanel, BorderLayout.CENTER);
@@ -43,62 +47,154 @@ public class AttendantInterface {
     	attendantFrame.pack();
     	attendantFrame.setVisible(true);
 	}
+	
+	
+	// Ends attendant interface session.
+	public void end_session() {
+		attendantFrame.setVisible(false);
+	}
 
 	// Main - only will be used for testing.
 	public static void main(String args[]) {
-		AttendantInterface A = new AttendantInterface(5);
-		A.notifyWeightDiscrepancy(1, 10.0);
+		AttendantInterface A = new AttendantInterface(20);
+		for (int i = 1; i <= 10; ++i) A.notifyBagApproval(i);
+		for (int i = 11; i <= 20; ++i) A.notifyWeightDiscrepancy(i, 10.0);
 	}
 	
 	
-	// Listener class used to listen for resolve button being pressed.
-	private class ResolveListener implements ActionListener {
+	//////////////////////
+	// BUTTON LISTENERS //
+	//////////////////////
+	
+	
+	// Listener class used to listen for overriding weight discrepancies.
+	private class OverrideListener implements ActionListener {
 
 		private int station_number;
 		
-		public ResolveListener(int n) {
-			// Listener needs to track interface and station number so that the
-			// interface can be properly updated. Will probably need to track the
-			// station too so that it can resolve weight discrepancy and continue checkout.
+		public OverrideListener(int n) {
 			station_number = n;
 		}
 		
 		
-		// When button is pressed...
+		@Override
 		public void actionPerformed(ActionEvent e) {
-			// When button is pressed, resolve weight discrepancy so that checkout
-			// can continue.
+			// When button is pressed, override weight discrepancy and continue checkout.
 			
-			// SOFTWARE: CALL SOMETHING TO RESOLVE WD.
+			// SOFTWARE: OVERRIDE WEIGHT DISCREPANCY - SET EXPECTED WEIGHT = ACTUAL
 			
 			// Set back to normal on interface.
-			discrepancyLabels[station_number - 1].setText("Station " + station_number + ": Working normally...");
-			// Lastly, de-register this listener and reset button text...
-			resolveButtons[station_number - 1].setText("");
-			resolveButtons[station_number - 1].removeActionListener(this);
+			deregisterButtonListeners(station_number);
 		}
 		
 	}
 	
 	
-	// Notifies attendant of weight discrepancy by adding a row to the
-	// attendant interface window containing station # and discrepancy,
-	// and will then either override the system so that the
-	// expected weight is now the current weight, or will get
-	// the customer to remove items so that the discrepancy is resolved.
-	// No matter what happens, when the discrepancy has been resolved
-	// and the expected weight has been updated the attendant will be able
-	// to press the "resolved" button in this row to continue the checkout.
+	// Listener class used for approve bags button.
+	private class ApproveBagsListener implements ActionListener {
+		
+		private int station_number;
+		
+		public ApproveBagsListener(int n) {
+			station_number = n;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// Approve bags...
+			
+			// SOFTWARE: UPDATE EXPECTED WEIGHT AND CONTINUE CHECKOUT.
+			
+			// Set back to normal on interface.
+			deregisterButtonListeners(station_number);
+			
+		}
+		
+	}
+	
+	// Listener class used for refuse bags button.
+	private class RefuseBagsListener implements ActionListener {
+		
+		private int station_number;
+		
+		public RefuseBagsListener(int n) {
+			station_number = n;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// Approve bags...
+			
+			// SOFTWARE: MAKE CALL TO ATTENDANT INTERFACE PROMPTING TO REMOVE BAGS.
+			
+			// Set back to normal on interface.
+			deregisterButtonListeners(station_number);
+			
+		}
+		
+	}
+	
+	
+	// Deregister all button listeners for a particular station.
+	// This is done whenever issues have been resolved and station should now be
+	// working normally.
+	public void deregisterButtonListeners(int station_number) {
+		discrepancyLabels[station_number - 1].setText("Station " + station_number + ": Working normally...");
+		// Lastly, de-register this listener and reset button text...
+		approveButtons[station_number - 1].setText("");
+		refuseButtons[station_number - 1].setText("");
+		for (int i = 0; i < refuseButtons[station_number - 1].getActionListeners().length; ++i) {
+			refuseButtons[station_number - 1].removeActionListener(refuseButtons[station_number - 1].getActionListeners()[i]);
+		}
+		for (int i = 0; i < approveButtons[station_number - 1].getActionListeners().length; ++i) {
+			refuseButtons[station_number - 1].removeActionListener(refuseButtons[station_number - 1].getActionListeners()[i]);
+		}
+	}
+	
+	
+	// ** METHOD TO BE CALLED BY SOFTWARE WHEN A WEIGHT DISCREPANCY IS DETECTED **
+	//
+	// Notifies attendant of weight discrepancy. Attendant can then either override
+	// the discrepancy, or can prompt the customer to remove items so that the discrepancy is resolved.
 	//
 	// NOTE: STATION NUMBER IS AN INT >= 1, MUST SUBTRACT ONE TO INDEX INTO COMPONENT ARRAYS.
 	public void notifyWeightDiscrepancy(int station_number, double discrepancy) {
 		discrepancyLabels[station_number - 1].setText("Station " + station_number
 				+ ": Weight discrepancy of " + discrepancy);
-		resolveButtons[station_number - 1].setText("Resolve");
+		approveButtons[station_number - 1].setText("Override");
 		
 		// Set up and add button listener.
-		ResolveListener rl = new ResolveListener(station_number);
-		resolveButtons[station_number - 1].addActionListener(rl);
+		OverrideListener rl = new OverrideListener(station_number);
+		approveButtons[station_number - 1].addActionListener(rl);
+	}
+	
+	
+	// ** METHOD TO BE CALLED BY BAGGING AREA LISTENER IF WEIGHT DISCREPANCY IS RESOLVED
+	// WITHOUT NEEDING AN OVERRIDE (e.g. customer removes problematic item) **
+	// 
+	// Resets row corresponding to station to default.
+	public void weightDiscrepancyResolved(int station_number) {
+		deregisterButtonListeners(station_number);
+	}
+	
+	
+	// ** METHOD TO BE CALLED BY SOFTWARE WHEN BAG APPROVAL IS NEEDED **
+	//
+	// Notifies attendant that customer is awaiting bag approval. Attendant can then
+	// either approve bags or refuse bags. If approve, call method in software that
+	// updates expected weight and continues checout. If refuse, then call method
+	// in customer interface to prompt customer to remove bags.
+	public void notifyBagApproval(int station_number) {
+		discrepancyLabels[station_number - 1].setText("Station " + station_number
+				+ ": Bag approval needed.");
+		approveButtons[station_number - 1].setText("Approve");
+		refuseButtons[station_number - 1].setText("Refuse");
+		
+		// Set up and add button listeners.
+		ApproveBagsListener abl = new ApproveBagsListener(station_number);
+		approveButtons[station_number - 1].addActionListener(abl);
+		RefuseBagsListener rbl = new RefuseBagsListener(station_number);
+		refuseButtons[station_number - 1].addActionListener(rbl);
 	}
 	
 }
