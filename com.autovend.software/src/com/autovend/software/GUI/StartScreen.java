6caba3a6f7fceab94b17dacdefec8a5630847398
Md Abdullah Mehedi Patwary <abdullahmehedi000@gmail.com>
2023-04-06 04:01:09 -0600

    start screen

--- com.autovend.software/src/com/autovend/software/GUI/StartScreen.java
+++ com.autovend.software/src/com/autovend/software/GUI/StartScreen.java
@@ -10,14 +10,12 @@ public class StartScreen {
     JPanel jPanel = new JPanel();
 
     public StartScreen(Controller controller){
-
         jPanel.setForeground(new Color(65, 73, 96));
         jPanel.setBackground(new Color(65, 73, 96));
         jPanel.setMinimumSize(new Dimension(1280, 720));
         jPanel.setMaximumSize(new Dimension(1280, 720));
         jPanel.setPreferredSize(new Dimension(1280, 720));
         jPanel.setSize(new Dimension(1280, 720));
-
         jPanel.setLayout(null);
 
         JButton goToMainScreen = new JButton("Start");
@@ -28,18 +26,26 @@ public class StartScreen {
             }
         });
         goToMainScreen.setOpaque(true);
-        goToMainScreen.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
         goToMainScreen.setBorder(new LineBorder(new Color(15,17,26),1, true));
         goToMainScreen.setBackground(new Color(142, 185, 151));
-        goToMainScreen.setBounds(500,332,280,55);
+
+
+        goToMainScreen.setBounds(500, 332, 280, 55);
         jPanel.add(goToMainScreen);
 
         JLabel title = new JLabel("Self Checkout Station");
-        title.setFont(new Font("Gotham", Font.BOLD, 60));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setFont(new Font("Apple Color Emoji", Font.BOLD, 60));
+
         title.setForeground(new Color(98, 145, 139));
-        title.setBounds(312,20,656,72);
+        title.setBounds(6,20,1268,300);
         jPanel.add(title);
 
 
+
     }
+
+
+
 }
