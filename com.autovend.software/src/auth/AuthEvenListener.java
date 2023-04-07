3d2943956f28991091e70b2cdc424f50dbc91f2b
liran0427 <106135733+liran0427@users.noreply.github.com>
2023-04-07 17:25:06 -0600

    Add successful and unsuccessful login/logout methods.

--- com.autovend.software/src/auth/AuthEvenListener.java
+++ com.autovend.software/src/auth/AuthEvenListener.java
@@ -4,8 +4,12 @@ import com.autovend.software.AbstractEventListener;
 
 public interface AuthEvenListener extends AbstractEventListener {
 
-	public void reactToLogIn(AttendantAccount attendantAccount);
+	public void reactToLogInSuccesfully(AttendantAccount attendantAccount);
 
-	public void reactToLogOut(AttendantAccount attendantAccount);
+	public void reactToLogOutSuccesfully(AttendantAccount attendantAccount);
+
+	public void reactToLogInUnsuccesfully(AttendantAccount attendantAccount);
+
+	public void reactToLogOutUnsuccesfully(AttendantAccount attendantAccount);
 
 }
