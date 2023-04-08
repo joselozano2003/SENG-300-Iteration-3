7abfb865fb2606235596916cdd921046197539c3
saradhuka01 <sara.dhuka01@gmail.com>
2023-04-08 13:42:05 -0600

    Correct import statement

--- com.autovend.software/src/gui/AttendantInterface.java
+++ com.autovend.software/src/gui/AttendantInterface.java
@@ -4,10 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.lsmr.selfcheckout.PriceLookupCode;
-import org.lsmr.selfcheckout.external.ProductDatabases;
-import org.lsmr.selfcheckout.products.PLUCodedProduct;
-
+import com.autovend.PriceLookUpCode;
+import com.autovend.products.PLUCodedProduct;
+import com.autovend.external.ProductDatabases;
 
 import javax.swing.*;
 
