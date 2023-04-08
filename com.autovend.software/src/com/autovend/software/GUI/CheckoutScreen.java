aac3b2dade775eda657a53ef6f673a40fc9ac328
Md Abdullah Mehedi Patwary <abdullahmehedi000@gmail.com>
2023-04-08 15:56:12 -0600

    payWith Card

--- com.autovend.software/src/com/autovend/software/GUI/CheckoutScreen.java
+++ com.autovend.software/src/com/autovend/software/GUI/CheckoutScreen.java
@@ -1,14 +1,24 @@
 package com.autovend.software.GUI;
 
 
+import com.autovend.Card;
+import com.autovend.GiftCard;
+import com.autovend.devices.SelfCheckoutStation;
+import com.autovend.external.CardIssuer;
+import com.autovend.software.customer.CustomerController;
 import com.autovend.software.customer.CustomerSession;
+import com.autovend.software.payment.GiftCardDatabase;
+import com.autovend.software.payment.PayWithCard;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.math.BigDecimal;
+import com.autovend.software.BankIO;
+import com.autovend.software.payment.PayWithGiftCard;
 
 public class CheckoutScreen extends JPanel{
 
@@ -16,8 +26,14 @@ public class CheckoutScreen extends JPanel{
     private  JTextArea infoText;
     private JButton returnAddItems;
     private JButton doneButton;
+    private com.autovend.devices.SelfCheckoutStation SelfCheckoutStation;
+    private String num;
     JPanel jPanel = new JPanel();
+
+    PayWithCard payWithCard = new PayWithCard(SelfCheckoutStation);
+    PayWithGiftCard payWithGiftCard = new PayWithGiftCard(SelfCheckoutStation);
     CustomerSession session = new CustomerSession();
+
     private BigDecimal amountEntered;
     public CheckoutScreen(){
 
@@ -58,7 +74,7 @@ public class CheckoutScreen extends JPanel{
         doneButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                session.loadStartGUI();
+                //session.loadStartGUI();
             }
         });
 
@@ -76,7 +92,7 @@ public class CheckoutScreen extends JPanel{
         returnAddItems.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                session.loadMainGUI();
+                //session.loadMainGUI();
             }
         });
         returnAddItems.setOpaque(true);
@@ -98,8 +114,51 @@ public class CheckoutScreen extends JPanel{
                             "Please Try Again!",
                             JOptionPane.ERROR_MESSAGE);
                     return;
+
                 }
-                //
+                if(BankIO.CARD_ISSUER_DATABASE.containsKey(creditCardNumber)) {
+                    CardIssuer card = BankIO.CARD_ISSUER_DATABASE.get(creditCardNumber);
+                    Object[] optionToPay = {"Tap", "Insert", "Swipe"};
+                    int cardPayType = JOptionPane.showOptionDialog(
+                            new JPanel(), "Please choose how you like to use your card",
+                            "Pay With Credit Card",
+                            JOptionPane.YES_NO_CANCEL_OPTION,
+                            JOptionPane.PLAIN_MESSAGE,
+                            null, optionToPay,
+                            optionToPay[0]
+                    );
+                    boolean transactionApproved = false;
+                    try {
+                        if (cardPayType == 0) {
+                            transactionApproved = payWithCard.tapCard(card);
+                        } else if (cardPayType == 1) {
+                            String pin = JOptionPane.showInputDialog("Please enter Card's PIN number: ", "");
+                            transactionApproved = payWithCard.insertCard(card, pin);
+                        } else if (cardPayType == 2) {
+                            transactionApproved = payWithCard.swipeCard(card, null);
+                        } else {
+                            JOptionPane.showMessageDialog(new JPanel(),
+                                    "You did not choose a valid method for payment", "Error!", JOptionPane.ERROR_MESSAGE);
+                        }
+
+
+                    } catch (Exception error) {
+                        transactionApproved = false;
+                    }
+                    if (transactionApproved) {
+                        amountEntered = amountEntered.add(session.getTotalCost());
+                        JOptionPane.showMessageDialog(new JPanel(),
+                                "Payment Successful", "Success!", JOptionPane.PLAIN_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(new JPanel(), "Payment Failed!", "Error!", JOptionPane.ERROR_MESSAGE);
+                    }
+                }else {
+                    JOptionPane.showMessageDialog(new JPanel(), "Card Data not found!", "Error!", JOptionPane.ERROR_MESSAGE);
+
+
+                }
+
+                update();
 
             }});
                 creditCard.setOpaque(true);
@@ -116,6 +175,56 @@ public class CheckoutScreen extends JPanel{
             @Override
             public void mouseClicked(MouseEvent e) {
 
+                String debitCardNumber = JOptionPane.showInputDialog("Please enter your Credit Card Number: ", "");
+                if(debitCardNumber.equals("")){
+                    JOptionPane.showMessageDialog(new JPanel(), "Invalid Inputs!",
+                            "Please Try Again!",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+
+                }
+                if(BankIO.CARD_ISSUER_DATABASE.containsKey(debitCardNumber)) {
+                    CardIssuer card = BankIO.CARD_ISSUER_DATABASE.get(debitCardNumber);
+                    Object[] optionToPay = {"Tap", "Insert", "Swipe"};
+                    int cardPayType = JOptionPane.showOptionDialog(
+                            new JPanel(), "Please choose how you like to use your card",
+                            "Pay With Debit Card",
+                            JOptionPane.YES_NO_CANCEL_OPTION,
+                            JOptionPane.PLAIN_MESSAGE,
+                            null, optionToPay,
+                            optionToPay[0]
+                    );
+                    boolean transactionApproved = false;
+                    try {
+                        if (cardPayType == 0) {
+                            transactionApproved = payWithCard.tapCard(card);
+                        } else if (cardPayType == 1) {
+                            String pin = JOptionPane.showInputDialog("Please enter Card's PIN number: ", "");
+                            transactionApproved = payWithCard.insertCard(card, pin);
+                        } else if (cardPayType == 2) {
+                            transactionApproved = payWithCard.swipeCard(card, null);
+                        } else {
+                            JOptionPane.showMessageDialog(new JPanel(),
+                                    "You did not choose a valid method for payment", "Error!", JOptionPane.ERROR_MESSAGE);
+                        }
+
+
+                    } catch (Exception error) {
+                        transactionApproved = false;
+                    }
+                    if (transactionApproved) {
+                        amountEntered = amountEntered.add(session.getTotalCost());
+                        JOptionPane.showMessageDialog(new JPanel(),
+                                "Payment Successful", "Success!", JOptionPane.PLAIN_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(new JPanel(), "Payment Failed!", "Error!", JOptionPane.ERROR_MESSAGE);
+                    }
+                }else {
+                    JOptionPane.showMessageDialog(new JPanel(), "Card Data not found!", "Error!", JOptionPane.ERROR_MESSAGE);
+                }
+
+                update();
+
 
             }});
         debitCard.setOpaque(true);
@@ -131,6 +240,54 @@ public class CheckoutScreen extends JPanel{
         giftCard.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                String giftCardNumber = JOptionPane.showInputDialog("Please enter your Gift Card Number: ", "");
+                if(giftCardNumber.equals("")){
+                    JOptionPane.showMessageDialog(new JPanel(), "Invalid Inputs!",
+                            "Please Try Again!",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+
+                }
+
+                if(GiftCardDatabase.getGiftCard(String num) == giftCardNumber) {
+                    CardIssuer card = BankIO.CARD_ISSUER_DATABASE.get(giftCardNumber);
+                    Object[] optionToPay = {"Insert", "Swipe"};
+                    int cardPayType = JOptionPane.showOptionDialog(
+                            new JPanel(), "Please choose how you like to use your gift card",
+                        "Pay With Gift Card",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.PLAIN_MESSAGE,
+                        null, optionToPay,
+                        optionToPay[0]
+                );
+                boolean transactionApproved = false;
+                try {
+                    if (cardPayType == 0) {
+                        String pin = JOptionPane.showInputDialog("Please enter Card's PIN number: ", "");
+                        transactionApproved = payWithGiftCard.insertCard(card, pin);
+                    } else if (cardPayType == 1) {
+                        transactionApproved = payWithGiftCard.swipeCard(card, null);
+                    } else {
+                        JOptionPane.showMessageDialog(new JPanel(),
+                                "You did not choose a valid method for payment", "Error!", JOptionPane.ERROR_MESSAGE);
+                    }
+
+
+                } catch (Exception error) {
+                    transactionApproved = false;
+                }
+                if (transactionApproved) {
+                    amountEntered = amountEntered.add(session.getTotalCost());
+                    JOptionPane.showMessageDialog(new JPanel(),
+                            "Payment Successful", "Success!", JOptionPane.PLAIN_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(new JPanel(), "Payment Failed!", "Error!", JOptionPane.ERROR_MESSAGE);
+                }
+            }else {
+                JOptionPane.showMessageDialog(new JPanel(), "Card Data not found!", "Error!", JOptionPane.ERROR_MESSAGE);
+            }
+
+            update();
 
 
             }});
@@ -213,7 +370,7 @@ public class CheckoutScreen extends JPanel{
 
 
     private String getInfoText(){
-        return "\n Total: " + session.getTotalCost() + "\n Entered: " + amountEntered.doubleValue() + "\n Changes Due: " + amountEntered.subtract(session.getTotalCost());
+        return "\n Total: " + session.getTotalCost().toString() + "\n Entered: " + amountEntered.doubleValue() + "\n Changes Due: " + amountEntered.subtract(session.getTotalCost());
     }
 
 }
