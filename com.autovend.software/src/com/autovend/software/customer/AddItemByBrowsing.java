2512e31b8643c3c53e7a7dd7a5ee9cc135db0b7e
benjaminfrey363 <109392270+benjaminfrey363@users.noreply.github.com>
2023-04-09 11:46:25 -0600

    Reformatting

--- com.autovend.software/src/com/autovend/software/customer/AddItemByBrowsing.java
+++ com.autovend.software/src/com/autovend/software/customer/AddItemByBrowsing.java
@@ -14,10 +14,10 @@ import javax.swing.text.*;
 
 public class AddItemByBrowsing {
 
-  static int MAX_ITEMS = 20;
+	static int MAX_ITEMS = 20;
 	
 	CustomerController controller;
-	
+
 	JFrame browsingFrame;
 	JPanel cataloguePanel;
 	JPanel letterPanel;
@@ -27,42 +27,43 @@ public class AddItemByBrowsing {
 	JButton previousCatalogue;
 	JButton itemButtons[] = new JButton[MAX_ITEMS];
 	int num_items;			// Actual number of items to display on screen.
-	
+
 	// Character array to represent string entered so far by user.
 	ArrayList<Character> stringEntered = new ArrayList<Character>();
-	
+
 	static char[] letters = {'A', 'B', 'C', 'D', 'E', 'F',
-    	    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-    	    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    JButton[] letterButtons = new JButton[letters.length];
+	    'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+	    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	JButton[] letterButtons = new JButton[letters.length];
+	
 	
 	// Called when add item by browsing button is pressed, this method will
 	// make calls to the controller to add items to shopping cart.
 	public void addItemByBrowsing() {
-    	//swing compounents needed for the GUI
-    	browsingFrame = new JFrame();
-    	cataloguePanel = new JPanel();
-    	letterPanel = new JPanel();
-    	previousPanel = new JPanel();
-    	nextPanel = new JPanel();
-    	
-    	JLabel Title = new JLabel();
-    	cataloguePanel.setLayout(new GridLayout(5,5, 2, 2)); 
-    	letterPanel.setLayout(new GridLayout(0,26, 2, 2));
-    	
-    	//frame set up 
-    	browsingFrame.setTitle("Add Item By Browsing");
-    	browsingFrame.setSize(1500,750);
-    	browsingFrame.setLayout(null);
-    	browsingFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    	
-    	//icon set up currently with a bug 
-    	ImageIcon logoIcon = new ImageIcon("logo.png");
-    	browsingFrame.setIconImage(logoIcon.getImage());
-    	browsingFrame.getContentPane().setBackground(Color.LIGHT_GRAY);
-    	
-    	 
-    	//Title for the page set up 
+		//swing compounents needed for the GUI
+		browsingFrame = new JFrame();
+		cataloguePanel = new JPanel();
+		letterPanel = new JPanel();
+		previousPanel = new JPanel();
+		nextPanel = new JPanel();
+
+		JLabel Title = new JLabel();
+		cataloguePanel.setLayout(new GridLayout(5,5, 2, 2)); 
+		letterPanel.setLayout(new GridLayout(0,26, 2, 2));
+
+		//frame set up 
+		browsingFrame.setTitle("Add Item By Browsing");
+		browsingFrame.setSize(1500,750);
+		browsingFrame.setLayout(null);
+		browsingFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+		//icon set up currently with a bug 
+		ImageIcon logoIcon = new ImageIcon("logo.png");
+		browsingFrame.setIconImage(logoIcon.getImage());
+		browsingFrame.getContentPane().setBackground(Color.LIGHT_GRAY);
+
+
+		//Title for the page set up 
 		Title.setText("Store Catalogue");
 		Title.setFont(new Font("Mv Boli",Font.PLAIN,20));
 		//Title.setVerticalAlignment(JLabel.TOP);
@@ -122,38 +123,39 @@ public class AddItemByBrowsing {
 	
 	
 	// Add alphabet to GUI, define button action.
-    private void addAlphabet() {
+    	private void addAlphabet() {
     	
-        for (int i = 0; i < letters.length; ++i) {
-            letterButtons[i] = new JButton(letters[i] + "");
-            // Define action of pressing button.
-            // The action of pressing a letter button is simply to add that
-            // letter to stringEntered!
-            letterButtonListener listener = new letterButtonListener(letters[i]);
-            // When button is pressed, character will be added to stringEntered.
-            letterButtons[i].addActionListener(listener);
-            letterPanel.add(letterButtons[i]);
-        }	
-        
-    }
+        	for (int i = 0; i < letters.length; ++i) {
+			letterButtons[i] = new JButton(letters[i] + "");
+			// Define action of pressing button.
+			// The action of pressing a letter button is simply to add that
+			// letter to stringEntered!
+			letterButtonListener listener = new letterButtonListener(letters[i]);
+			// When button is pressed, character will be added to stringEntered.
+			letterButtons[i].addActionListener(listener);
+			letterPanel.add(letterButtons[i]);
+        	}	
+		
+    	}
     
-    private void addCatalogue() {
-    	
-    	//Catalogue Next and Previous Buttons 
-    	nextCatalogue = new JButton(">");
-    	nextCatalogue.setBounds(0, 0, 75, 130);
-    	
-    	previousCatalogue = new JButton("<");
-    	previousCatalogue.setBounds(0, 0, 75, 130);
-    	
-    	nextPanel.add(nextCatalogue);
-    	previousPanel.add(previousCatalogue);
-        	
-    	//add the items
-    	for(int j = 1; j < num_items; j++) {
-    		cataloguePanel.add(itemButtons[j]);
-      }	
-    }
+    	private void addCatalogue() {
+    
+		//Catalogue Next and Previous Buttons 
+		nextCatalogue = new JButton(">");
+		nextCatalogue.setBounds(0, 0, 75, 130);
+
+		previousCatalogue = new JButton("<");
+		previousCatalogue.setBounds(0, 0, 75, 130);
+
+		nextPanel.add(nextCatalogue);
+		previousPanel.add(previousCatalogue);
+
+		//add the items
+		for(int j = 1; j < num_items; j++) {
+			cataloguePanel.add(itemButtons[j]);
+		}
+		
+    	}
   
   
 }
