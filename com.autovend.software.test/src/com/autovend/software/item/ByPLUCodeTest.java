79ed5f13e9de8f41e637a230010496afffdb36fe
Akansha <96041473+akansha-malik@users.noreply.github.com>
2023-04-11 17:26:41 -0600

    Item test updates: ByScanning and ByPLUCode
    ByScanning test: gets 100% coverage, but not sure what do with disable/enable events
    
    ByPLUCode test: gets 86.2% coverage, there will be an update to a method in this class. So I will alter the test cases when the changes are committed.

--- com.autovend.software.test/src/com/autovend/software/item/ByPLUCodeTest.java
+++ com.autovend.software.test/src/com/autovend/software/item/ByPLUCodeTest.java
@@ -1,11 +1,17 @@
 package com.autovend.software.item;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.autovend.devices.ElectronicScale;
 import com.autovend.devices.SelfCheckoutStation;
 import com.autovend.products.PLUCodedProduct;
+import com.autovend.products.Product;
 import com.autovend.software.test.Setup;
 import com.autovend.software.ui.CustomerView;
 
@@ -43,7 +49,7 @@ public class ByPLUCodeTest {
 	}
 	
 	/**
-	 * Test ByPLUCode constructor with null parameter.
+	 * Test ByPLUCode constructor with null station parameter.
 	 */
 	@Test (expected = NullPointerException.class)
 	public void testContructorNullStation() {
@@ -51,14 +57,115 @@ public class ByPLUCodeTest {
 	}
 	
 	/**
-	 * Test the event when a PLU Code has been entered 
+	 * Test ByPLUCode constructor with null customer view parameter.
+	 */
+	@Test (expected = NullPointerException.class)
+	public void testContructorNullView() {
+		new ByPLUCode(station, null);
+	}
+	
+	/**
+	 * Tests the processing of a single PLUCoded Product
 	 */
 	@Test
-	public void testReactToPLUCodeEnteredEvent() {
-		PLUCodedProduct PLUProd = Setup.createPLUProduct1234(11.99, 3, true);
+	public void testSingleValidPLUProcessing() {
+		int expected = 1;
+		PLUCodedProduct PLUProd123 = Setup.createPLUProduct1234(11.99, 3, true);
 		
-		//byPLUCode.reactToPLUCodeEnteredEvent(PLUProd.getPLUCode(), 3);
+		byPLUCode.reactToPLUCodeEntered("1234");
 		
+		instance.register(new ItemListenerStub() {
+			@Override
+			public void onItemAddedEvent(Product product, double quantity) {
+				found++;
+				if (found == 1) {
+					assertEquals(PLUProd123, product);
+				}
+			}});
+		
+		byPLUCode.processPLUInput("1234", 2);
+		assertEquals(expected, found);
+	}
+	
+	/**
+	 * Tests the processing of a multiple PLUCoded Products
+	 */
+	@Test
+	public void testMultipleValidPLUProcessing() {
+		int expected = 2;
+		PLUCodedProduct PLUProd123 = Setup.createPLUProduct1234(11.99, 3, true);
+		PLUCodedProduct PLUProd456 = Setup.createPLUProduct56789(3.57, 1, true);
+		
+		instance.register(new ItemListenerStub() {
+			@Override
+			public void onItemAddedEvent(Product product, double quantity) {
+				found++;
+				if (found == 1) {
+					assertEquals(PLUProd123, product);
+				}
+				
+				if (found == 2) {
+					assertEquals(PLUProd456, product);
+				}
+			}});
+		
+		byPLUCode.processPLUInput("1234", 2);
+		byPLUCode.processPLUInput("56789", 1);
+		assertEquals(expected, found);
+	}
+	
+	/*
+	 * Attempt to getting a PLUCodedProduct with a null PLUCode but it won't be possible..
+	 */
+	
+	/*
+	@Test
+	public void testValidAndInvalidPLUProcessing() {
+		int expected = 2;
+		PLUCodedProduct PLUProd123 = Setup.createPLUProduct1234(11.99, 3, true);
+		PLUCodedProduct PLUProd456 = Setup.createPLUProduct56789(3.57, 1, false);
+		
+		instance.register(new ItemListenerStub() {
+			@Override
+			public void onItemAddedEvent(Product product, double quantity) {
+				found++;
+				if (found == 1) {
+					assertEquals(PLUProd123, product);
+				}
+			}
+			
+			@Override 
+			public void onItemNotFoundEvent() {
+				found++;
+		}});
+		
+		byPLUCode.processPLUInput("1234", 2);
+		byPLUCode.processPLUInput("56789", 1);
+		
+		assertEquals(expected, found);
+	}
+	*/
+	
+	/**
+	 * Tests the event when the weight is changed during a PLUCode scan
+	 */
+	@Test
+	public void testEventReactToWeightChanged() {
+		int expected = 1;
+		PLUCodedProduct PLUProd123 = Setup.createPLUProduct1234(11.99, 3, true);
+		byPLUCode.reactToPLUCodeEntered("1234");
+		
+		instance.register(new ItemListenerStub() {
+			@Override
+			public void onItemAddedEvent(Product product, double quantity) {
+				found++;
+				if (found == 1) {
+					assertEquals(PLUProd123, product);
+				}
+			}});
+		
+		byPLUCode.reactToWeightChangedEvent(station.scale, 6);
+		assertEquals(expected, found);
 	}
 	
 	/**
@@ -72,6 +179,8 @@ public class ByPLUCodeTest {
 		byPLUCode.reactToDisabledEvent(station.scale);
 		byPLUCode.reactToOverloadEvent(station.scale);
 		byPLUCode.reactToOutOfOverloadEvent(station.scale);
+		
+		assertEquals(byPLUCode.getListeners(), instance.getListeners());	
 	}
 
 }
