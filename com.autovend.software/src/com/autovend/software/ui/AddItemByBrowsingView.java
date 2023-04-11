397311c31d93acf1e604fe3f369cc9882adbb257
Jaxon Sloan <101297325+jsloan3@users.noreply.github.com>
2023-04-11 00:51:45 -0600

    Adds functionality + support with ProductsDatabase2

--- com.autovend.software/src/com/autovend/software/ui/AddItemByBrowsingView.java
+++ com.autovend.software/src/com/autovend/software/ui/AddItemByBrowsingView.java
@@ -1,21 +1,38 @@
 package com.autovend.software.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
+import com.autovend.Barcode;
+import com.autovend.Numeral;
 import com.autovend.external.ProductDatabases;
+import com.autovend.products.BarcodedProduct;
 import com.autovend.products.PLUCodedProduct;
+import com.autovend.products.Product;
+import com.autovend.software.item.ItemEventListener;
+import com.autovend.software.item.ProductsDatabase2;
 
 public class AddItemByBrowsingView extends JPanel {
 	
@@ -26,17 +43,23 @@ public class AddItemByBrowsingView extends JPanel {
 		// Maximum number of items per screen.
 		static int MAX_ITEMS = 20;
 		
+		private List<ItemEventListener> observers;
+		
 		JPanel browsingPanel = this;
-		JPanel mainPanel;
+		JPanel middlePanel;
 		JPanel cataloguePanel;
 		JPanel letterPanel;
+		JScrollPane scrollBar;
 		JPanel previousPanel;
 		JPanel nextPanel;
 		JButton nextButton;
 		JButton previousButton;
 		JLabel searchLabel;
+		JPanel borderPanel;
 		JButton itemButtons[] = new JButton[MAX_ITEMS];
 		int num_items = 0;			// Actual number of items to display on screen.
+		
+	
 		
 		// Character array to represent string entered so far by user.
 		ArrayList<Character> stringEntered = new ArrayList<Character>();
@@ -61,45 +84,105 @@ public class AddItemByBrowsingView extends JPanel {
 		// make calls to the controller to add items to shopping cart.
 		public AddItemByBrowsingView() {
 	    	//swing compounents needed for the GUI
-			mainPanel = new JPanel();
+			
+			Map<JButton, Product> productMap = ProductsDatabase2.Products_In_Visual_Catalogue_Database;
+			
 	    	cataloguePanel = new JPanel();
+	    	middlePanel = new JPanel();
+	    	borderPanel = new JPanel();
 	    	letterPanel = new JPanel();
 	    	previousPanel = new JPanel();
 	    	nextPanel = new JPanel();
+	    	scrollBar = new JScrollPane(borderPanel);
+	    	//scrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	    	
+	    	JButton button1 = new JButton();
+	    	JButton button2 = new JButton();
+	    	JButton button3 = new JButton();
+	    	JButton button4 = new JButton();
+	    	JButton button5 = new JButton();
+	    	JButton button6 = new JButton();
+	    	JButton button7 = new JButton();
+	    	JButton button8 = new JButton();
+	    	
+	    	browsingPanel.setLayout(new BoxLayout(browsingPanel,BoxLayout.PAGE_AXIS));
+	    	middlePanel.setLayout(new BoxLayout(middlePanel,BoxLayout.LINE_AXIS));
+	    	borderPanel.setLayout(new BorderLayout());
 	    	
 	    	JLabel Title = new JLabel();
 	    	searchLabel = new JLabel("");
 	    	// Item buttons will be placed in the catalogue panel.
-	    	cataloguePanel.setLayout(new GridLayout(5,5, 2, 2)); 
-	    	letterPanel.setLayout(new GridLayout(0,26, 2, 2));
-	    	
-	    	//frame set up 
-	    	browsingPanel.setLayout(null);
+	    	cataloguePanel.setLayout(new FlowLayout()); 
 	    	
 	    	//icon set up currently with a bug 
 	    	ImageIcon logoIcon = new ImageIcon("logo.png"); 
 	    	//Title for the page set up 
 			Title.setText("Store Catalogue");
 			Title.setFont(new Font("Mv Boli",Font.PLAIN,20));
-			//Title.setVerticalAlignment(JLabel.TOP);
-			//Title.setHorizontalAlignment(JLabel.CENTER);
-			Title.setBounds(650, 25, 200, 50); //set x,y position within a frame as well as dimensions
+
 			
 			cataloguePanel.setBackground(Color.WHITE);
-			cataloguePanel.setBounds(100,300,1200,400);
+			
+			cataloguePanel.setAlignmentX(browsingPanel.CENTER_ALIGNMENT);
+			
+			cataloguePanel.setPreferredSize(new Dimension(1200,5000));
+			scrollBar.setPreferredSize(new Dimension(1200,400));
+			
+			Title.setAlignmentX(browsingPanel.CENTER_ALIGNMENT);
 			
 			
 
 			//adding everything to the main frame 
 			//browsingFrame.pack();
+			borderPanel.add(cataloguePanel);
+			browsingPanel.add(Box.createVerticalStrut(50));
 			browsingPanel.add(Title);
+			browsingPanel.add(Box.createVerticalStrut(30));
 			browsingPanel.add(searchLabel);
-			browsingPanel.add(cataloguePanel);
+			browsingPanel.add(middlePanel);
+			middlePanel.add(Box.createGlue());
+			middlePanel.add(scrollBar);
+			middlePanel.add(Box.createGlue());
+			browsingPanel.add(Box.createGlue());
 			browsingPanel.setVisible(true);
+			
+			for (JButton currentButton : productMap.keySet()) {
+				
+				Product productGot = productMap.get(currentButton);
+				
+				cataloguePanel.add(currentButton);
+				currentButton.setPreferredSize(new Dimension(400,200));
+				
+				if (productGot instanceof BarcodedProduct) {
+					BarcodedProduct barProductGot = (BarcodedProduct) productGot;
+					currentButton.setText(barProductGot.getDescription());
+				}
+				
+				
+				currentButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						notifyProductButtonPressedEvent(productGot);
+					}
+				});
+			}
+
 		}
 		
 		public static void main(String[] args) {
-	    	JFrame frame = new JFrame();
+	    	
+			
+			Barcode testBarcode = new Barcode(Numeral.one, Numeral.one, Numeral.one, Numeral.one);
+			BarcodedProduct testProduct = new BarcodedProduct(testBarcode, "apple", new BigDecimal(10), 10);
+			JButton testButton = new JButton();
+			ProductsDatabase2.Products_In_Visual_Catalogue_Database.put(testButton, testProduct);
+			
+			Barcode testBarcode2 = new Barcode(Numeral.two, Numeral.one, Numeral.one, Numeral.one);
+			BarcodedProduct testProduct2 = new BarcodedProduct(testBarcode2, "banana", new BigDecimal(10), 10);
+			JButton testButton2 = new JButton();
+			ProductsDatabase2.Products_In_Visual_Catalogue_Database.put(testButton2, testProduct2);
+			
+			JFrame frame = new JFrame();
 	    	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 			frame.setUndecorated(true);
@@ -107,5 +190,15 @@ public class AddItemByBrowsingView extends JPanel {
 			frame.add(new AddItemByBrowsingView());
 			frame.validate();
 	    }
+		
+		public void notifyProductButtonPressedEvent(Product product) {
+			for (ItemEventListener listener : observers) {
+				listener.onItemAddedEvent(product, 1);
+			}
+		}
+		
+		public void register(ItemEventListener listener) {
+			observers.add(listener);
+		}
 
 }
