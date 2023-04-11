848fe52d956cccaf4917355198ce321e5b708f45
Khondaker Samin Rashid <77379456+Tusher7@users.noreply.github.com>
2023-04-11 12:30:01 -0600

    Added testcases for customer session
    Added testcases for customer session.

--- com.autovend.software.test/src/com/autovend/software/customer/CustomerSessionTest.java
+++ com.autovend.software.test/src/com/autovend/software/customer/CustomerSessionTest.java
@@ -56,23 +56,184 @@ public class CustomerSessionTest {
 		barcodedProduct = Setup.createBarcodedProduct123(12, 5, true);
 	}
 
+	//TESTING PAYMENT-COMPLETION.
+		//Customer pays the full amount for the one item.
+		@Test
+		public void isPaymentCompleteTestOneItem() {
+			//One item has been added to cart. Customer needs to pay 12 dollars (Since 1 item is 12 dollars).
+			session.addItemToCart(barcodedProduct, 1);
+					
+			session.addPayment(BigDecimal.valueOf(12));					
+			// Check if payment is marked as complete
+			assertTrue(session.isPaymentComplete());
+		}
+		//Customer doesn't pay the full amount for the one item.
+		@Test
+		public void isPaymentIncompleteTestoneItem() {
+			//One item have been added to cart. Customer needs to pay 12 dollars (Since 1 item is 12 dollars).
+			session.addItemToCart(barcodedProduct, 2);
+					
+			session.addPayment(BigDecimal.valueOf(1));	//Customer should pay 12 dollars, but he paid 1 dollar!				
+			// Check if payment is marked as incomplete
+			assertFalse(session.isPaymentComplete()); //So Payment isn't complete.
+		}
+				
 
-	@Test
-	public void isPaymentCompleteTest() {
-		CustomerSession session = new CustomerSession();
-		session.addPayment(BigDecimal.valueOf(20));
-		session.addPayment(BigDecimal.valueOf(15));
+		//Customer pays the full amount for the two items.
+		@Test
+		public void isPaymentCompleteTestTwoItems() {
+			//Two items have been added to cart. Customer needs to pay 24 dollars (Since 1 item is 12 dollars).
+			session.addItemToCart(barcodedProduct, 2);
+				
+			session.addPayment(BigDecimal.valueOf(24));					
+			// Check if payment is marked as complete
+			assertTrue(session.isPaymentComplete());
+		}
 		
-		// Check if payment is marked as complete
-		assertTrue(session.isPaymentComplete());
+		//Customer doesn't pay the full amount for the two items.
+		@Test
+		public void isPaymentIncompleteTestTwoItems() {
+			//Two items have been added to cart. Customer needs to pay 24 dollars (Since 1 item is 12 dollars).
+			session.addItemToCart(barcodedProduct, 2);
+				
+			session.addPayment(BigDecimal.valueOf(23));	//Customer should pay 24 dollars, but he paid 23 dollars!				
+			// Check if payment is marked as incomplete
+			assertFalse(session.isPaymentComplete()); //So Payment isn't complete.
+		}
+		//Customer pays the full amount for the 10 items.
+		@Test
+		public void isPaymentCompleteTestTenItems() {
+			//Ten items have been added to cart. Customer needs to pay 120 dollars (Since 1 item is 12 dollars).
+			session.addItemToCart(barcodedProduct, 10);
+					
+			session.addPayment(BigDecimal.valueOf(120));					
+			// Check if payment is marked as complete
+			assertTrue(session.isPaymentComplete());
+		}
+		//Customer doesn't pay the full amount for the ten items.
+		@Test
+		public void isPaymentIncompleteTestTenItems() {
+			//Two items have been added to cart. Customer needs to pay 120 dollars (Since 1 item is 12 dollars).
+			session.addItemToCart(barcodedProduct, 10);
+					
+			session.addPayment(BigDecimal.valueOf(119));//Customer should pay 120 dollars, but he paid 119 dollars!				
+			// Check if payment is marked as incomplete
+			assertFalse(session.isPaymentComplete()); //So Payment isn't complete.
+			}
 		
-		// Add another payment and check if payment is still complete
-		session.addPayment(BigDecimal.valueOf(5));
-		assertTrue(session.isPaymentComplete());
+		//TESTING CUSTOMER PAYMENT (addPayment method)
 		
-		// Add payment less than the total cost and check if payment is incomplete
-		session.addPayment(BigDecimal.valueOf(10));
-		assertFalse(session.isPaymentComplete());
-	}
+		//Making Payment on 1 item
+		@Test
+		public void customerPaymentTestOneItem() {
+			
+			session.addItemToCart(barcodedProduct, 1); // 12 dollar worth product
+			
+			//Before payment, total paid amount is 0	
+			assertEquals(BigDecimal.valueOf(0), session.getTotalPaid());
+			
+			//After making payment
+			session.addPayment(BigDecimal.valueOf(10));
+			assertEquals(BigDecimal.valueOf(10), session.getTotalPaid());
+			 
+			
+			//Making full payment afterwards
+			session.addPayment(BigDecimal.valueOf(2));
+			assertEquals(BigDecimal.valueOf(12), session.getTotalPaid());	
+		}
+		
+		//Making Payment on 2 items
+		@Test
+		public void customerPaymentTestTwoItems() {
+			
+			session.addItemToCart(barcodedProduct, 2); // 12 dollar worth product
+				
+			//Before payment, total paid amount is 0	
+			assertEquals(BigDecimal.valueOf(0), session.getTotalPaid());
+				
+			//After making payment
+			session.addPayment(BigDecimal.valueOf(10));
+			assertEquals(BigDecimal.valueOf(10), session.getTotalPaid());
+				 
+				
+			//Making full payment afterwards
+			session.addPayment(BigDecimal.valueOf(14));
+			assertEquals(BigDecimal.valueOf(24), session.getTotalPaid());	
+			}
+		
+		//Making Payment on 20 items
+		@Test
+		public void customerPaymentTestTwentyItems() {
+				
+			session.addItemToCart(barcodedProduct, 20); // 12 dollar worth product
+					
+			//Before payment, total paid amount is 0	
+			assertEquals(BigDecimal.valueOf(0), session.getTotalPaid());
+					
+			//After making payment
+			session.addPayment(BigDecimal.valueOf(200));
+			assertEquals(BigDecimal.valueOf(200), session.getTotalPaid());
+					 
+					
+			//Making full payment afterwards
+			session.addPayment(BigDecimal.valueOf(40));
+			assertEquals(BigDecimal.valueOf(240), session.getTotalPaid());	
+			}
+		
+		//TESTING THAT THE CHANGE DUE CALCULATED IS CORRECT OR NOT
+		
+		/* Test Case: Paying 40 dollars for 36 dollar items and checking if changeDue is calculated correctly.
+		 * 
+		 * Description: If the customer pays more than the total cost, the customer will be paid back the money they 
+		 * are owed.
+		 * 
+		 * Expected Result: The chnageDue calculated for the customer is 4 dollars.
+		 */
+		@Test
+		public void changeDueCalculationTestFourDollars() {
+			session.addItemToCart(barcodedProduct, 3); // 12 dollar worth product
+			BigDecimal expected = new BigDecimal("4.00");
+
+			session.addPayment(BigDecimal.valueOf(40));
+			assertEquals(expected, session.getChangeDue());
+		}
+		/* Test Case: Paying 25 dollars for 24 dollar items and checking if changeDue is calculated correctly.
+		 * 
+		 * Description: If the customer pays more than the total cost, the customer will be paid back the money they 
+		 * are owed.
+		 * 
+		 * Expected Result: The chnageDue calculated for the customer is 1 dollar.
+		 */
+		@Test
+		public void changeDueCalculationTestOneDollar() {
+			session.addItemToCart(barcodedProduct, 2); // 12 dollar worth product
+			BigDecimal expected = new BigDecimal("1.00");
+
+			session.addPayment(BigDecimal.valueOf(25));
+			assertEquals(expected, session.getChangeDue());
+		}
+		/* Test Case: Paying 70 dollars for 60 dollar items and checking if changeDue is calculated correctly.
+		 * 
+		 * Description: If the customer pays more than the total cost, the customer will be paid back the money they 
+		 * are owed.
+		 * 
+		 * Expected Result: The chnageDue calculated for the customer is 10 dollars.
+		 */
+		@Test
+		public void changeDueCalculationTestTenDollar() {
+			session.addItemToCart(barcodedProduct, 5); // 12 dollar worth product
+			BigDecimal expected = new BigDecimal("10.00");
+
+			session.addPayment(BigDecimal.valueOf(70));
+			assertEquals(expected, session.getChangeDue());
+		}
+		
+		
+		
+		@Test
+		public void addItemToCartTest() {
+			
+		}
+		
 
 	}
