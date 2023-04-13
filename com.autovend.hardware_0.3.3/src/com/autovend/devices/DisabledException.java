78585487432b6b5a21ef9d33fb098f386ba4924c
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-04-12 22:54:18 -0600

    Update Hardware

--- com.autovend.hardware_0.3.3/src/com/autovend/devices/DisabledException.java
+++
@@ -1,8 +0,0 @@
-package com.autovend.devices;
-
-/**
- * Issued when a device cannot perform its usual functions because it has been
- * disabled.
- */
-@SuppressWarnings("serial")
-public class DisabledException extends RuntimeException {}
